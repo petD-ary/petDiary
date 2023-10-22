24f0044b7d3a2cb13defc80e1fdb697cf1cedd25
Dorabang <psa1195@naver.com>
2023-10-22 16:55:38 +0900

    design: account 헤더 컴포넌트 스타일

---
+++ app/components/Account/Heading/Styled.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const TitleWrapper = styled.div`
+  height: 100px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #f0f0f0;
+  padding: 0 48px;
+
+  & h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
+export const IconWrapper = styled.div`
+  width: 28px;
+  height: 28px;
+  overflow: hidden;
+  position: absolute;
+  left: 48px;
+  bottom: 20px;
+  cursor: pointer;
+
+  img {
+    padding: 6px;
+  }
+`;
