0446d72bff6239bf229c29e1f68a8a3267acce3d
Dorabang <psa1195@naver.com>
2023-10-25 20:41:15 +0900

    chore: styled 컴포넌트 분리

--- app/(home)/account/page.tsx
+++ app/(home)/account/page.tsx
@@ -1,37 +1,10 @@
 'use client';
 
-import styled from 'styled-components';
 import Breadcrumb from '@/components/Account/Breadcrumb';
 import UserForm from '@/components/Account/UserForm';
 import { useState } from 'react';
 import Link from 'next/link';
-
-const Container = styled.div`
-  width: 488px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-bottom: 150px;
-
-  & button[type='submit'] {
-    width: 100%;
-    background: #000;
-    color: #fff;
-    font-weight: 600;
-    border: none;
-    border-radius: 8px;
-    padding: 20px 0;
-    margin-bottom: 64px;
-  }
-
-  & p > a {
-    padding-left: 6px;
-    text-decoration: underline;
-    font-weight: 600;
-  }
-`;
+import { Container } from './styled';
 
 const AccountPage = () => {
   const [step, setStep] = useState(0);
@@ -41,14 +14,6 @@ const AccountPage = () => {
       <Breadcrumb step={step} />
 
       {step === 0 && <UserForm />}
-      {step === 1 && <UserForm />}
-      {step === 2 && <UserForm />}
-
-      <button type='submit' onClick={() => setStep((prev) => prev + 1)}>
-        {step === 0 ? '다음 단계로' : ''}
-        {step === 1 ? '가입하기' : ''}
-        {step === 2 ? '시작하기' : ''}
-      </button>
 
       <p>
         이미 계정이 있으신가요?<Link href='/login'>로그인</Link>
