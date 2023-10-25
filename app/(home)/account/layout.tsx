2b99e14458c7299f290be8866b896e40fcce8451
Dorabang <psa1195@naver.com>
2023-10-25 19:09:29 +0900

    feat: account Header를 이용해 AccountLayout 컴포넌트 생성

---
+++ app/(home)/account/layout.tsx
@@ -0,0 +1,12 @@
+import Heading from '@/components/Account/Heading';
+
+const AccountLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Heading title='회원가입' />
+      {children}
+    </div>
+  );
+};
+
+export default AccountLayout;
