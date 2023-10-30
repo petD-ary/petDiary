a22717d41e46726cae9f3be2a3ac7c13339aceb7
Dorabang <psa1195@naver.com>
2023-10-30 15:24:25 +0900

    feat: 품종 모달창 상태 추가

--- app/recoil/atoms.ts
+++ app/recoil/atoms.ts
@@ -4,3 +4,8 @@ export const stepState = atom<number>({
   key: 'stepState',
   default: 0,
 });
+
+export const variantModalState = atom<boolean>({
+  key: 'variantModalState',
+  default: true,
+});
