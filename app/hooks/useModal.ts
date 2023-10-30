a7d230b3ecc89eb3d18afaa8ee19e83d7d439e47
Dorabang <psa1195@naver.com>
2023-10-30 15:39:45 +0900

    feat: 모달 커스텀훅스 생성
    - 모달창 밖 영역 클릭 시 닫히는 기능

---
+++ app/hooks/useModal.ts
@@ -0,0 +1,22 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function useModal(
+  ref: React.RefObject<HTMLDivElement>,
+  handler: (event: any) => void
+) {
+  useEffect(() => {
+    const listener = (event: { target: any }) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
