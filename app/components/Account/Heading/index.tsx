7a6b74b7ef988b95d7160ff8e6d3e98489450305
Dorabang <psa1195@naver.com>
2023-11-02 22:20:10 +0900

    feat: 단계 상태에 따른 조건부 뒤로가기 버튼 기능 추가

--- app/components/Account/Heading/index.tsx
+++ app/components/Account/Heading/index.tsx
@@ -1,30 +1,43 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React from "react";
-import { IconWrapper, TitleWrapper } from "./styled";
-import Image from "next/image";
-import backBtn from "@/assets/images/backBtn.png";
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { IconWrapper, TitleWrapper } from './styled';
+import Image from 'next/image';
+import backBtn from '@/assets/images/backBtn.png';
+import { useRecoilState } from 'recoil';
+import { stepState } from '@/recoil/atoms';
 
 const Heading = ({ title }: { title: string }) => {
   const router = useRouter();
+  const [step, setStep] = useRecoilState(stepState);
+
+  const handleClickRoute = () => {
+    if (step > 0 && step <= 2) {
+      return setStep((prev) => prev - 1);
+    } else if (step === 0) {
+      return router.back();
+    }
+  };
 
   return (
-    <TitleWrapper title={title}>
-      {title === "회원가입" ? (
-        <IconWrapper onClick={() => router.back()}>
-          <Image
-            src={backBtn}
-            alt="뒤로 가기"
-            fill
-            sizes="100%"
-            style={{ objectFit: "contain" }}
-            priority
-          />
-        </IconWrapper>
-      ) : null}
+    <div>
+      <TitleWrapper title={title}>
+        {title === '회원가입' ? (
+          <IconWrapper onClick={() => handleClickRoute()}>
+            <Image
+              src={backBtn}
+              alt='뒤로 가기'
+              fill
+              sizes='100%'
+              style={{ objectFit: 'contain' }}
+              priority
+            />
+          </IconWrapper>
+        ) : null}
 
-      <h2>{title}</h2>
-    </TitleWrapper>
+        <h2>{title}</h2>
+      </TitleWrapper>
+    </div>
   );
 };
 
