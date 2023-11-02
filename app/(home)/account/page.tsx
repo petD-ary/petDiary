d7b3014f3f95e76adf13267001e10b984b61fc11
Dorabang <psa1195@naver.com>
2023-11-02 20:39:16 +0900

    chore: 불필요한 임시 데이터 삭제

--- app/(home)/account/page.tsx
+++ app/(home)/account/page.tsx
@@ -15,24 +15,6 @@ const AccountPage = () => {
   const step = useRecoilValue(stepState);
   const isOpen = useRecoilValue(variantModalState);
 
-  const variantData = [
-    { id: '0', title: '노르웨이숲' },
-    { id: '1', title: '니벨룽' },
-    { id: '2', title: '데본 렉스' },
-    { id: '3', title: '도이치 랭하' },
-    { id: '4', title: '돈스코이' },
-    { id: '5', title: '라이코이' },
-    { id: '6', title: '랙돌' },
-    { id: '7', title: '러시안 블루' },
-    { id: '8', title: '맨덜레이' },
-    { id: '9', title: '맹크스' },
-    { id: '10', title: '맹크스' },
-    { id: '11', title: '맹크스' },
-    { id: '12', title: '맹크스' },
-    { id: '14', title: '맹크스' },
-    { id: '15', title: '맹크스' },
-  ];
-
   return (
     <Container>
       <Breadcrumb step={step} />
