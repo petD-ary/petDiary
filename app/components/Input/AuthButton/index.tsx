8a40f9242ef2d3ecda95f14e79231397ddfa42f5
Dorabang <psa1195@naver.com>
2023-11-02 23:53:55 +0900

    design: 버튼 컴포넌트 disabled 시 css 추가

--- app/components/Input/AuthButton/index.tsx
+++ app/components/Input/AuthButton/index.tsx
@@ -1,33 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
 
 interface AuthButtonProps {
-  type: "button" | "submit" | "reset";
+  type: 'button' | 'submit' | 'reset';
   content: string;
   onClick?: () => void;
   disabled: boolean;
+  css?: string;
 }
 
-// const StyledButton = styled.button`
-//   width: 100%;
-//   height: 60px;
-//   color: #fff;
-//   background-color: #000;
-//   border-radius: 8px;
-//   margin-top: 60px;
-//   &:disabled {
-//     opacity: 0.5;
-//     cursor: default;
-//   }
-// `;
-
-function AuthButton({ type, content, onClick, disabled }: AuthButtonProps) {
+function AuthButton({
+  type,
+  content,
+  onClick,
+  disabled,
+  css,
+}: AuthButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className="w-full py-9 px-8 text-white bg-black mt-20 disabled:opacity-50 disabled:cursor-not-allowed rounded-xl"
+      className={`
+      w-full h-[60px]
+      text-white bg-black
+      rounded-lg
+      mt-20
+      ${
+        disabled
+          ? 'disabled:opacity-50 disabled:cursor-not-allowed'
+          : 'hover:bg-grayColor-300 active:bg-grayColor-400'
+      }
+      transition-colors
+      ${css}
+      `}
     >
       {content}
     </button>
