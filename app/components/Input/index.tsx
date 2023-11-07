8bc740efb9af888374a2cffffd2cbe9c0b85fdf1
Dorabang <psa1195@naver.com>
2023-11-07 19:01:40 +0900

    feat: 비밀번호 표시 기능 추가

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -1,3 +1,6 @@
+import { BsCheckLg, BsXLg } from 'react-icons/bs';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+
 interface InputProps {
   label: string;
   type: string;
@@ -7,6 +10,9 @@ interface InputProps {
   placeholder?: string;
   button?: string;
   desc?: string;
+  checkbox?: boolean;
+  handleChangeType?: () => void;
+  showPassword?: boolean;
 }
 
 const Input = ({
@@ -18,22 +24,51 @@ const Input = ({
   placeholder,
   button,
   desc,
+  ...props
 }: InputProps) => {
   return (
     <div className='w-full mt-5 text-1.4rem'>
       <label className='block p-2'>{label}</label>
-      <div className='flex justify-between gap-2'>
+      <div className='flex justify-between gap-2 relative'>
         <input
           type={type}
           value={value}
           onChange={(e) => setValue(e.target.value)}
           required={required}
           placeholder={placeholder && placeholder}
-          className={`flex-grow outline-none border-none p-5 bg-grayColor-100 placeholder-black rounded-xl
+          className={`flex-grow outline-none border-none p-5 bg-grayColor-100 placeholder-grayColor-400 rounded-xl
           ${type === 'text' ? 'cursor-text' : ''}
           ${type === 'password' ? 'cursor-text' : ''}
           `}
         />
+        {props.checkbox ? (
+          <BsCheckLg
+            className='absolute right-5 top-1/2 -translate-y-1/2 text-green-500'
+            size={18}
+          />
+        ) : (
+          props.checkbox !== undefined && (
+            <BsXLg
+              size={18}
+              className='absolute right-5 top-1/2 -translate-y-1/2 text-rose-500'
+            />
+          )
+        )}
+        {props.showPassword ? (
+          <AiOutlineEyeInvisible
+            size={18}
+            onClick={props.handleChangeType}
+            className='absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer'
+          />
+        ) : (
+          props.showPassword !== undefined && (
+            <AiOutlineEye
+              size={18}
+              onClick={props.handleChangeType}
+              className='absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer'
+            />
+          )
+        )}
         {button ? (
           <button
             type='button'
@@ -47,7 +82,7 @@ const Input = ({
           </button>
         ) : null}
       </div>
-      {desc ? <p className='pt-5 pl-5'>{desc}</p> : null}
+      {desc ? <p className='pt-5 pl-5 text-grayColor-400'>{desc}</p> : null}
     </div>
   );
 };
