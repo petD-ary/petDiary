8db2209411430bc4b224d72bdab03e5ebda8be2e
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-11-09 01:12:02 +0900

    design:schedule component생성 scheduleList -> scheduleItem -> scheduleItemDetail 단위

--- app/components/Main/Schedule/ScheduleList.tsx
+++ app/components/Main/Schedule/ScheduleList.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Temperatures from "../Weather/Temperatures";
+import ScheduleItem from "./ScheduleItem";
+import ScheduleItemDetail from "./ScheduleItemDetail";
 
 const ScheduleList = () => {
-  return <div>ScheduleList</div>;
+  return (
+    <div>
+      <ScheduleItem
+        date="23"
+        day="토요일"
+        detail={
+          <ScheduleItemDetail work="사상충 약 먹으러!" location="프렌즈동물병원" time="14:00 ~ 15:00" />
+        }
+      />
+    </div>
+  );
 };
 
 export default ScheduleList;
--- app/components/Main/Schedule/index.tsx
+++ app/components/Main/Schedule/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Calender from "./Calender";
+import ScheduleList from "./ScheduleList";
 
 const Schedule = () => {
   return (
-    <div className="w-3/6 p-4 bg-grayColor-100 rounded-xl ">
-      <div className="mb-4 text-lg font-semibold">이달의 일정</div>
+    <div className="flex flex-col gap-5 w-3/6 p-4 bg-grayColor-100 rounded-xl ">
+      <div className="text-lg font-semibold">이달의 일정</div>
       <Calender />
+      <ScheduleList />
     </div>
   );
 };
