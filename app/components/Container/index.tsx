5bf5b0189895e08db5012fb73e2ce205f2a2623c
Dorabang <psa1195@naver.com>
2023-11-20 22:40:40 +0900

    design: 반응형 사이즈 추가

--- app/components/Container/index.tsx
+++ app/components/Container/index.tsx
@@ -1,6 +1,9 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='mx-2 sm:mx-auto px-0 sm:px-6 lg:px-12 h-full'>
+    <div
+      className='mx-2 sm:mx-auto px-0 sm:px-6 lg:px-20
+    h-full lg:w-[calc(100%_-_110px)]'
+    >
       {children}
     </div>
   );
