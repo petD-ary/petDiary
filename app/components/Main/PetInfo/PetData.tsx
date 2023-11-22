a1bed2330322459c989d5a927f72da11d4c2a795
Dorabang <psa1195@naver.com>
2023-11-22 21:47:31 +0900

    chore: image src 오류로 인해 잠시 주석처리

--- app/components/Main/PetInfo/PetData.tsx
+++ app/components/Main/PetInfo/PetData.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import DDay from "./DDay";
-import Highlight from "./Highlight";
-import PetImg from "./PetImg";
+import React from 'react';
+import DDay from './DDay';
+import Highlight from './Highlight';
+import PetImg from './PetImg';
 const PetData = () => {
   return (
-    <div className="flex justify-center mt-2">
+    <div className='flex justify-center mt-2'>
       <div>
         <DDay />
-        <PetImg />
-        <div className="text-center">
-          <span className="bg-white px-5 py-2 rounded-2xl text-sm">
-            <Highlight content="김콩" />
+        {/* <PetImg /> */}
+        <div className='text-center'>
+          <span className='bg-white px-5 py-2 rounded-2xl text-sm'>
+            <Highlight content='김콩' />
           </span>
         </div>
       </div>
