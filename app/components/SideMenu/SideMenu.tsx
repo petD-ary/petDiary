217d2ce0b117749ec4b8fc1d819b459cd7028dcd
Dorabang <psa1195@naver.com>
2023-11-22 14:31:36 +0900

    design: 전체적으로 사이즈 조정 및 flex 옵션 수정

--- app/components/SideMenu/SideMenu.tsx
+++ app/components/SideMenu/SideMenu.tsx
@@ -1,34 +1,49 @@
-import NavBtn from "./Menu";
+import NavBtn from './Menu';
 
 const SideMenu = () => {
   let nav = [
     {
-      title: "스탬프",
-      link: "",
+      title: '스탬프',
+      link: '/stemp/walk',
     },
     {
-      title: "캘린더",
-      link: "",
+      title: '캘린더',
+      link: '#',
     },
     {
-      title: "반려스토어",
-      link: "",
+      title: '반려스토어',
+      link: '/store/shop',
     },
     {
-      title: "커뮤니티",
-      link: "",
+      title: '커뮤니티',
+      link: '#',
     },
   ];
   return (
-    <div className="flex flex-col justify-between w-1/12 h-screen py-20 rounded-ee-3xl rounded-se-3xl bg-grayColor-100">
-      <div className="flex flex-col gap-5">
-        <div className="w-8 h-8 mx-auto mb-1 rounded-2xl bg-grayColor-200"></div>
-        {nav.map((item, i) => {
-          return <NavBtn key={item.tilte} navItem={item.title} icon="icon" />;
-        })}
+    <div
+      className='fixed z-10 flex flex-col justify-between
+    lg:w-[110px] h-screen py-20
+    rounded-ee-3xl rounded-se-3xl bg-grayColor-100'
+    >
+      <div className='flex flex-col gap-6 items-center'>
+        <div className='w-20 pb-6 border-b border-grayColor-400'>
+          <div className='w-8 h-8 mx-auto mb-1 rounded-2xl bg-grayColor-200'></div>
+        </div>
+        <div className='flex flex-col gap-6 items-center'>
+          {nav.map((item) => {
+            return (
+              <NavBtn
+                key={item.title}
+                navItem={item.title}
+                icon='icon'
+                link={item.link}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div>
-        <NavBtn navItem="홈" icon="icon" />
+      <div className='flex flex-col items-center'>
+        <NavBtn navItem='홈' icon='icon' link='/' />
       </div>
     </div>
   );
