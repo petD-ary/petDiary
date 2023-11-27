dd98dbbfb78edecbe1482f45326b2ffbd6d39095
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-11-28 00:56:06 +0900

    feat: 캘린더 생성

--- app/components/Main/Schedule/Calender.tsx
+++ app/components/Main/Schedule/Calender.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import {
+  format,
+  addMonths,
+  subMonths,
+  startOfMonth,
+  endOfMonth,
+  eachWeekOfInterval,
+  isToday,
+  isSameMonth,
+} from "date-fns";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-const Calender = () => {
-  return <div className="p-4 ">날짜</div>;
+const Calendar = () => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const nextMonth = () => setCurrentMonth(addMonths(currentMonth, 1));
+  const prevMonth = () => setCurrentMonth(subMonths(currentMonth, 1));
+
+  const weeksInMonth = eachWeekOfInterval({
+    start: startOfMonth(currentMonth),
+    end: endOfMonth(currentMonth),
+  });
+
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-4">
+        <div className="px-2 py-1 rounded" onClick={prevMonth}>
+          <IoIosArrowBack />
+        </div>
+        <span className="text-xl font-semibold">{format(currentMonth, "MMMM yyyy")}</span>
+        <button className=" px-2 py-1 rounded" onClick={nextMonth}>
+          <IoIosArrowForward />
+        </button>
+      </div>
+      <table className="w-full border-collapse">
+        <thead>
+          <tr>
+            {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
+              <th key={day} className="border p-2 text-center font-semibold">
+                {day}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {weeksInMonth.map((week, index) => (
+            <tr key={index}>
+              {eachWeekOfInterval({ start: week, end: week }).map((day) => (
+                <td
+                  key={day.toString()}
+                  className={`p-2 text-center ${
+                    isToday(day)
+                      ? "bg-blue-200"
+                      : isSameMonth(day, currentMonth)
+                      ? "bg-white"
+                      : "bg-gray-200"
+                  }`}
+                >
+                  {format(day, "d")}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default Calender;
+export default Calendar;
