808196fc61a03315ca0b20c92ff0ae7f10f7fccb
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-11-28 00:38:06 +0900

    chore:dayMark mockData 생성

--- app/components/Main/Schedule/Calender.tsx
+++ app/components/Main/Schedule/Calender.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 const Calender = () => {
-  return (
-    <div className=" bg-white rounded-xl">
-      <div className="p-4 flex justify-between font-semibold border-b border-grayColor-100">
-        <div>20일, 수요일</div>
-        <div>9월 2023년</div>
-      </div>
-      <div className="p-4 ">날짜</div>
-    </div>
-  );
+  return <div className="p-4 ">날짜</div>;
 };
 
 export default Calender;
--- app/components/Main/WeatherCard/WeatherCard.tsx
+++ app/components/Main/WeatherCard/WeatherCard.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 interface CardType {
   title: string;
   weather: string;
-
   addition?: React.ReactNode;
 }
 
