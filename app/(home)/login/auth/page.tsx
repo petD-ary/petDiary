8d0fbbaaeb802aca9d4bc701877be9f9915ba2f6
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-12-10 00:24:13 +0900

    feat:인가코드 post

--- app/(home)/login/auth/page.tsx
+++ app/(home)/login/auth/page.tsx
@@ -2,13 +2,26 @@
 
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+const axios = require("axios");
 
 const Redirection = () => {
+  let KAKAO_AUTH_CODE = new URL(window.location.href).searchParams.get("code");
   const router = useRouter();
+  //   인가 코드 전송
+  async function postCode() {
+    try {
+      const response = await axios.post("주소", { KAKAO_AUTH_CODE });
+      router.push("/");
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
-    let KAKAO_AUTH_CODE = new URL(window.location.href).searchParams.get("code");
-    router.push("/");
+    postCode();
   }, []);
+
   return <div>로그인중</div>;
 };
 
