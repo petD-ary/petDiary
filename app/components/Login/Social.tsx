47f3e6fec5b4da2653ae1d790c7773ba9adfbfdb
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-12-24 18:43:52 +0900

    refactor:env 수정

--- app/components/Login/Social.tsx
+++ app/components/Login/Social.tsx
@@ -1,5 +1,5 @@
 // 카카오톡
-const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
+const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
 const KAKAO_CALLBACK_URL = process.env.KAKAO_CALLBACK_URL;
 const kakaolink = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_CALLBACK_URL}&response_type=code`;
 
@@ -8,18 +8,18 @@ export const kakaoLogin = () => {
 };
 
 // 구글
-const GOOGLE_REST_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-const GOOGLE_CALLBACK_URL = process.env.KAKAO_CALLBACK_URL;
-const googlelink = `https://kauth.kakao.com/oauth/authorize?client_id=${GOOGLE_REST_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&response_type=code`;
+const GOOGLE_REST_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
+const googlelink = `https://accounts.google.com/o/oauth2/v2/auth?scope=profile&response_type=code&client_id=${GOOGLE_REST_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}`;
 
 export const googleLogin = () => {
   window.location.href = googlelink;
 };
 
 // 네이버
-const NAVER_REST_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-const NAVER_CALLBACK_URL = process.env.KAKAO_CALLBACK_URL;
-const naverlink = `https://kauth.kakao.com/oauth/authorize?client_id=${NAVER_REST_CLIENT_ID}&redirect_uri=${NAVER_CALLBACK_URL}&response_type=code`;
+const NAVER_REST_CLIENT_ID = process.env.NAVER_CLIENT_ID;
+const NAVER_CALLBACK_URL = process.env.NAVER_CALLBACK_URL;
+const naverlink = `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_REST_CLIENT_ID}&redirect_uri=${NAVER_CALLBACK_URL}&response_type=code`;
 
 export const naverLogin = () => {
   window.location.href = naverlink;
