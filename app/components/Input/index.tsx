0cfe3e591f8ef85d4fc2053563ad8c68b7b798f4
Dorabang <psa1195@naver.com>
2023-12-26 20:49:43 +0900

    refactor: compound components pattern 으로 input 컴포넌트 리팩토링
    - 부모 컴포넌트의 제어권이 높아짐

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -1,41 +1,103 @@
-interface InputProps {
-  label: string;
-  type: string;
-  value: string;
-  setValue: (value: string) => void;
-  required?: boolean;
-  placeholder?: string;
-  desc?: string;
-}
+'use client';
+import React, { Children, cloneElement } from 'react';
+import {
+  ButtonProps,
+  DateInputProps,
+  InputProps,
+  LabelProps,
+  TextInputProps,
+} from './type';
 
-const Input = ({
-  label,
-  type,
-  value,
-  setValue,
-  required,
-  placeholder,
-  desc,
-}: InputProps) => {
+const Input = ({ children, onClick, onChange, value }: InputProps) => {
   return (
-    <div className='w-full mt-5 text-1.4rem'>
-      <label className='block p-2'>{label}</label>
-      <div className='flex justify-between gap-2 relative'>
-        <input
-          type={type}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          required={required}
-          placeholder={placeholder && placeholder}
-          className={`flex-grow outline-none border-none p-5 bg-grayColor-100 placeholder-grayColor-400 rounded-xl
-          ${type === 'text' ? 'cursor-text' : ''}
-          ${type === 'password' ? 'cursor-text' : ''}
-          `}
-        />
-      </div>
-      {desc ? <p className='pt-5 pl-5 text-grayColor-400'>{desc}</p> : null}
+    <div>
+      {Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          const newProps = {
+            onClick,
+            value,
+            onChange,
+          };
+
+          return cloneElement(child, newProps);
+        }
+        return null;
+      })}
     </div>
   );
 };
+
+const InputClass = 'w-full p-4 mt-2 rounded-lg';
+
+const Label = ({ children, ...rest }: LabelProps) => (
+  <label className={`flex gap-1`}>
+    {children}
+    {rest.isRequired ? <span className='text-error'>*</span> : null}
+  </label>
+);
+
+const TextInput = ({ value, onChange, ...rest }: TextInputProps) => (
+  <input
+    type='text'
+    value={value}
+    onChange={onChange}
+    className={`${InputClass} border border-text-dividers focus:border-text-border transition-colors`}
+    {...rest}
+  />
+);
+
+const DateInput = ({ value, onChange, ...rest }: DateInputProps) => (
+  <input
+    type='date'
+    value={value}
+    onChange={onChange}
+    className={`${InputClass}`}
+    {...rest}
+  />
+);
+
+const Button = ({
+  type = 'button',
+  children,
+  onClick,
+  isDisabled,
+  ...rest
+}: ButtonProps) => (
+  <button
+    type={type}
+    onClick={onClick}
+    disabled={isDisabled}
+    {...rest}
+    className={`w-full border rounded-lg text-center py-5
+    ${
+      rest.variant === 'contained'
+        ? 'bg-primary-500 text-white border-primary-500 disabled:text-white disabled:bg-grayColor-200 disabled:border-grayColor-200'
+        : ''
+    }
+    ${
+      rest.variant === 'outlined'
+        ? 'bg-white text-primary-500 border-primary-700 disabled:border-grayColor-200 disabled:text-grayColor-200'
+        : ''
+    }
+    `}
+  >
+    {children}
+  </button>
+);
+
+const Success = ({ children }: LabelProps) => (
+  <p className='text-success leading-secondary pl-[3px] pt-[6px]'>{children}</p>
+);
+
+const Error = ({ children }: LabelProps) => (
+  <p className='text-error'>{children}</p>
+);
+
+Input.Label = Label;
+Input.TextInput = TextInput;
+Input.DateInput = DateInput;
+Input.Success = Success;
+Input.Error = Error;
+Input.Button = Button;
 
 export default Input;
