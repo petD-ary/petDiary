5b682e265105c8eec26351f804f2d872fe41c2eb
Dorabang <psa1195@naver.com>
2023-12-26 19:25:56 +0900

    chore: 로그인 화면 UI 및 코드 변경진행 중

--- app/(home)/account/layout.tsx
+++ app/(home)/account/layout.tsx
@@ -3,7 +3,7 @@ import Heading from '@/components/Account/Heading';
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Heading title='회원가입' />
+      <Heading />
       {children}
     </div>
   );
---
+++ app/components/Account/Heading/index copy.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import backBtn from '@/assets/images/backBtn.png';
+import { useRecoilState } from 'recoil';
+import { stepState } from '@/recoil/Account/atoms';
+
+const Heading = ({ title }: { title: string }) => {
+  const router = useRouter();
+  const [step, setStep] = useRecoilState(stepState);
+
+  const handleClickRoute = () => {
+    if (step > 0 && step <= 2) {
+      return setStep((prev) => prev - 1);
+    } else if (step === 0) {
+      return router.back();
+    }
+  };
+
+  return (
+    <div
+      className={`
+      h-[100px] w-full px-12 relative
+      ${title === '회원가입' ? 'border-b border-b-grayColor-100' : ''}
+      `}
+    >
+      {title === '회원가입' ? (
+        <div
+          onClick={() => handleClickRoute()}
+          className='w-[28px] h-[28px] absolute [&_img]:p-[6px] left-[48px] bottom-5 cursor-pointer'
+        >
+          <Image
+            src={backBtn}
+            alt='뒤로 가기'
+            fill
+            sizes='100%'
+            style={{ objectFit: 'contain' }}
+            priority
+          />
+        </div>
+      ) : null}
+
+      <h2 className='text-[20px] font-bold absolute bottom-5 left-1/2 -translate-x-1/2'>
+        {title}
+      </h2>
+    </div>
+  );
+};
+
+export default Heading;
