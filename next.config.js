e24a834f68bf28813e3ec700ad999082d5a1de30
Dorabang <psa1195@naver.com>
2023-12-26 19:19:17 +0900

    feat: api 주소 변경으로 인한 nextConfig 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:5001/api/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
