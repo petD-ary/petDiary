083a3c62346a130203c60737d86f6e2ffced2e97
Dorabang <psa1195@naver.com>
2023-12-26 20:31:47 +0900

    feat: className 추가를 위한 props 생성

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -2,10 +2,15 @@ import { FC, ReactNode } from 'react';
 
 interface ContainerProps {
   children: ReactNode;
+  addClass?: string;
 }
 
-const Container: FC<ContainerProps> = ({ children }) => {
-  return <div className='w-full md:max-w-3xl mx-auto px-5'>{children}</div>;
+const Container: FC<ContainerProps> = ({ children, addClass }) => {
+  return (
+    <div className={`w-full md:max-w-3xl mx-auto px-5 ${addClass}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
