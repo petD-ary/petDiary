048040b640cf4d1f5ad555d5b8412ee24714179c
Dorabang <psa1195@naver.com>
2023-12-27 22:53:47 +0900

    chore: 변경된 Heading 컴포넌트 적용

--- app/(home)/login/layout.tsx
+++ app/(home)/login/layout.tsx
@@ -1,9 +1,12 @@
-import Heading from "@/components/Account/Heading";
+import Heading from '@/components/Account/Heading';
 
 const LoginLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Heading title="로그인" />
+      <Heading
+        title='로그인'
+        subTitle='로그인을 진행하고 펫 다이어리를 이용해 보세요'
+      />
       {children}
     </div>
   );
