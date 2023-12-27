1d2d5b254661c36e98e8b2258d10819808633466
Dorabang <psa1195@naver.com>
2023-12-27 22:56:06 +0900

    refactor: ccp가 적용된 Input 컴포넌트로 Account 페이지 리팩토링
    - 현재 이메일 중복 검사관련 로직은 api가 발행되는대로 추가할 예정

--- app/components/Account/UserForm/index.tsx
+++ app/components/Account/UserForm/index.tsx
@@ -1,55 +1,91 @@
 'use client';
 import Input from '@/components/Input';
-import AuthButton from '@/components/Input/AuthButton';
 import { authObjState, stepState } from '@/recoil/Account/atoms';
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
-import InputButton from '@/components/Input/InputButton';
-import ShowInput from '@/components/Input/ShowInput';
-import InputCheck from '@/components/Input/InputCheck';
+import Heading from '../Heading';
+import axios from '@/libs/axios';
+import Button from '@/components/Button';
 
 const UserForm = () => {
   const setStep = useSetRecoilState(stepState);
 
   const [nickname, setNickname] = useState('');
   const [error, setError] = useState<string | null>(null);
+  // const [success, setSuccess] = useState<string | null>(null);
   const [checkNickname, setCheckNickname] = useState(false);
+  const [validNickname, setValidNickname] = useState<any[]>([]);
 
-  const handleCheckNickname = () => {};
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // setStep((prev) => prev + 1);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
   };
 
-  const handleCheckedAvailability = async () => {};
+  const handleCheckNickname = (str: string) => {
+    let checkSpc = /[~!@#$%^&*()_+|<>?:{}]/;
+
+    if (checkSpc.test(str)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    /* axios.get('/users').then((result) => {
+      setCheckNickname(result.data.length === 0 ? true : false);
+      const data = [result.data];
+      setValidNickname(data);
+    }); */
+
+    if (handleCheckNickname(nickname)) {
+      setError('특수문자 ~!@#$%^&*()_제외');
+    }
+    // else if (validNickname.length > 0) {
+    // setError('이미 등록되어 있는 닉네임 입니다');
+    // }
+    else {
+      setError(null);
+    }
+
+    /* 다음단계로 */
+    if (/* checkNickname && */ error === null) {
+      setStep((prev) => prev + 1);
+    }
+  };
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)} className='w-full pt-6 pb-16'>
-      <Input>
-        <Input.Label isRequired>닉네임</Input.Label>
-        <Input.TextInput
-          value={nickname}
-          onChange={(e) => setNickname(e.target.value)}
-          placeholder='닉네임을 입력해 주세요'
-        />
-        <Input.Button
-          isDisabled={nickname === '' ? true : false}
-          variant='outlined'
-          onClick={handleCheckNickname}
-        >
-          중복체크
-        </Input.Button>
-        <Input.Button
-          isDisabled={!checkNickname ? true : false}
-          variant='contained'
-          type='submit'
-        >
-          다음
-        </Input.Button>
-      </Input>
-    </form>
+    <>
+      <div className='pt-4 pb-6 flex flex-col gap-4'>
+        <div></div>
+        <Heading title='닉네임 설정' subTitle='닉네임을 입력해 주세요' />
+      </div>
+
+      <form onSubmit={(e) => handleSubmit(e)} className='w-full pt-6 pb-16'>
+        <div className='flex flex-col pt-6 pb-3'>
+          <Input onChange={handleChange} value={nickname} error={error}>
+            <Input.Label isRequired>닉네임</Input.Label>
+            <Input.TextInput
+              error={error !== null ? true : false}
+              placeholder='닉네임을 입력해 주세요'
+            />
+            <Input.Error>{error}</Input.Error>
+            {/* <Input.Success>{success}</Input.Success> */}
+          </Input>
+        </div>
+
+        <div className='flex flex-col pt-6 pb-3'>
+          <Button
+            isDisabled={nickname !== '' ? false : true}
+            variant='contained'
+            type='submit'
+          >
+            다음
+          </Button>
+        </div>
+      </form>
+    </>
   );
 };
 
