b05bb78c2f8b343dd463dda7b2d23d46d7b2cb8e
Dorabang <psa1195@naver.com>
2023-12-27 22:50:04 +0900

    fix: width, height 미설정으로 인한 Image 오류 해결

--- app/components/Splash.tsx
+++ app/components/Splash.tsx
@@ -7,7 +7,7 @@ const Splash = () => {
       <p className='text-white text-sm font-medium leading-primary'>
         집사의 감동을 넘어 반려동물의 감동으로
       </p>
-      <Image src={Logo.src} alt='Pet diary logo' />
+      <Image src={Logo.src} alt='Pet diary logo' width={150} height={35} />
     </div>
   );
 };
