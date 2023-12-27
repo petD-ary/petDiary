b39c183a2092ec02229066fb2bd31c28d5ef0a72
Dorabang <psa1195@naver.com>
2023-12-27 22:59:16 +0900

    test: Header 컴포넌트 테스트 진행 중

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,25 +1,48 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+import IconX from '@/assets/images/Icon-x.svg';
+import IconLeft from '@/assets/images/icon-left.svg';
 
-const Header = () => {
+import { useRecoilState } from 'recoil';
+import { stepState } from '@/recoil/Account/atoms';
+import IconBtnWrapper from '../Button/IconBtnWrapper';
+
+interface HeaderProps {
+  type: 'logo' | 'two-icon' | 'default' | 'account';
+}
+
+const Header = ({ type }: HeaderProps) => {
+  return (
+    <header className='w-full md:max-w-3xl mx-auto px-2 py-1'>
+      {type === 'account' ? <AccountHeader /> : null}
+      {type === 'two-icon' ? <div></div> : null}
+    </header>
+  );
+};
+
+const AccountHeader = () => {
   const router = useRouter();
+  const [step, setStep] = useRecoilState(stepState);
+
+  const handleClickRoute = () => {
+    if (step > 0 && step <= 2) {
+      return setStep((prev) => prev - 1);
+    } else if (step === 0) {
+      return router.back();
+    }
+  };
 
   return (
-    <div
-      className='w-[calc(100%_-_96px)] h-[100px]
-    mx-auto flex justify-between items-center'
-    >
-      <h1 className='text-lg px-[6px] py-[2px]'>
-        <Link href='/'>Logo</Link>
-      </h1>
-      <button
-        className='px-5 py-[10px] bg-black text-white
-        hover:opacity-80 transition-opacity'
-        onClick={() => router.push('/account')}
-      >
-        회원가입
-      </button>
+    <div className={`flex ${step !== 0 ? 'justify-between' : 'justify-end'}`}>
+      {step !== 0 ? (
+        <button type='button' onClick={handleClickRoute} className='p-4'>
+          <IconLeft />
+        </button>
+      ) : null}
+
+      <IconBtnWrapper onClick={() => router.push('/login')}>
+        <IconX />
+      </IconBtnWrapper>
     </div>
   );
 };
