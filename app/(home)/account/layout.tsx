c4925226c64ebe2f6a00ea43eda4ea36f4fedd86
Dorabang <psa1195@naver.com>
2023-12-27 22:50:39 +0900

    feat: 잘못된 Heading 컴포넌트 사용으로 인해 Header 컴포넌트로 변경

--- app/(home)/account/layout.tsx
+++ app/(home)/account/layout.tsx
@@ -1,10 +1,12 @@
 import Heading from '@/components/Account/Heading';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
 
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <Heading />
-      {children}
+      <Header type='account' />
+      <Container>{children}</Container>
     </div>
   );
 };
