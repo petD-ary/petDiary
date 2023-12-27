12772f4820f1e63c1bbcb337614983998cbf56aa
Dorabang <psa1195@naver.com>
2023-12-27 22:56:46 +0900

    chore: Account 페이지 마크업 구조 변경 진행 중

--- app/(home)/account/page.tsx
+++ app/(home)/account/page.tsx
@@ -4,34 +4,22 @@ import Breadcrumb from '@/components/Account/Breadcrumb';
 import UserForm from '@/components/Account/UserForm';
 
 import { useRecoilValue } from 'recoil';
-import Link from 'next/link';
 import { stepState } from '@/recoil/Account/atoms';
 
 import { PetInForm } from '@/components/Account/PetInfoForm';
 import ComplatedPage from '@/components/Account/ComplatedPage';
+import Container from '@/components/Container';
 
 const AccountPage = () => {
   const step = useRecoilValue(stepState);
 
   return (
-    <div
-      className='w-[488px] mx-auto pb-[150px]
-    flex flex-col justify-center items-center'
-    >
-      <Breadcrumb step={step} />
+    <div className=''>
+      {/* <Breadcrumb step={step} /> */}
 
       {step === 0 && <UserForm />}
       {step === 1 && <PetInForm />}
       {step === 2 && <ComplatedPage />}
-
-      {step === 0 && (
-        <p className='text-sm'>
-          이미 계정이 있으신가요?
-          <Link href='/login' className='pl-[6px] underline font-semibold'>
-            로그인
-          </Link>
-        </p>
-      )}
     </div>
   );
 };
