b1bf72dffea3166d2f2e58961262be1e8e64cc4d
Dorabang <psa1195@naver.com>
2023-12-29 02:52:58 +0900

    chore: 회원가입 완료 페이지 진행 중

--- app/components/Account/ComplatedPage.tsx
+++ app/components/Account/ComplatedPage.tsx
@@ -1,42 +1,27 @@
 'use client';
-import Image from 'next/image';
-// import AuthButton from '../Input/AuthButton';
 import { useRouter } from 'next/navigation';
+import AccountComplated from '@/assets/images/account/account-complated.svg';
+import Title1 from '../Typography/Title1';
+import Body2 from '../Typography/Body2';
+import Button from '../Button';
 
 const ComplatedPage = () => {
   const router = useRouter();
 
   return (
-    <div
-      className='w-full flex flex-col justify-center items-center
-    pt-[162px] text-center
-    '
-    >
-      <div className='relative w-[200px] h-[200px]'>
-        <Image
-          src=''
-          alt=''
-          fill
-          sizes='100%'
-          style={{ objectFit: 'contain' }}
-          priority
-        />
+    <div>
+      <div>
+        <AccountComplated />
       </div>
-
-      <h3 className='text-[32px] font-semibold pt-12 pb-3'>
-        회원가입이 완료되었습니다.
-      </h3>
-      <p>
-        사랑하는 반려동물과의 행복한 나날들
-        <br />펫 다이어리와 함께 해보아요!
-      </p>
-
-      {/* <AuthButton
-        type='button'
-        content='시작하기'
-        onClick={() => router.push('/login')}
-        disabled={false}
-      /> */}
+      <div className='flex flex-col gap-3 items -center'>
+        <Title1>회원가입이 완료되었습니다</Title1>
+        <Body2 className='-tracking-[0.375px]'>
+          사랑하는 반려동물과의 행복한 나날들 펫 다이어리와 함께 해보아요!
+        </Body2>
+      </div>
+      <Button variant='contained' onClick={() => router.push('/')}>
+        시작하기
+      </Button>
     </div>
   );
 };
