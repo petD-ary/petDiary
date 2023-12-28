7e421f055dda1096d19b2882225da3b5740f7056
Dorabang <psa1195@naver.com>
2023-12-28 21:36:51 +0900

    desing: 글자 행간 수정

--- app/components/Splash.tsx
+++ app/components/Splash.tsx
@@ -4,7 +4,7 @@ import Logo from '@/assets/images/logo/pd_logo_white.png';
 const Splash = () => {
   return (
     <div className='bg-primary-500'>
-      <p className='text-white text-sm font-medium leading-primary'>
+      <p className='text-white text-sm font-medium leading-[1.1]'>
         집사의 감동을 넘어 반려동물의 감동으로
       </p>
       <Image src={Logo.src} alt='Pet diary logo' width={150} height={35} />
