97dba735ea2da4bfea4f954e57bb6e9fbe29d3ae
Dorabang <psa1195@naver.com>
2023-12-29 02:52:30 +0900

    chore: checkbox 인풋 타입 추가

--- app/components/Input/type.ts
+++ app/components/Input/type.ts
@@ -1,13 +1,15 @@
+import { ReactNode } from 'react';
+
 export interface InputProps {
   children: JSX.Element | JSX.Element[] | null;
   onClick?: () => void;
-  onChange?: (value: any) => void;
+  onChange?: (value?: any) => void;
   value?: string;
   error?: string | null;
 }
 
 export interface LabelProps {
-  children: React.ReactNode;
+  children?: ReactNode;
   isRequired?: boolean;
 }
 
@@ -19,6 +21,12 @@ export interface TextInputProps {
 }
 
 export interface DateInputProps {
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  onChange?: (value?: any) => void;
+}
+
+export interface CheckInputProps {
+  children?: ReactNode;
+  id: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
