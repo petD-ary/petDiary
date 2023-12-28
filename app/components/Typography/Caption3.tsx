2adb4653201935c9c8a421b33c523fd1f88f9b4f
Dorabang <psa1195@naver.com>
2023-12-28 21:26:54 +0900

    remove: 불필요한 파일 삭제

--- app/components/Typography/Caption3.tsx
+++
@@ -1,9 +0,0 @@
-import { TypographyProps } from './type';
-
-const Caption3 = ({ children, className }: TypographyProps) => {
-  return (
-    <p className={`text-caption2 tracking-normal ${className}`}>{children}</p>
-  );
-};
-
-export default Caption3;
