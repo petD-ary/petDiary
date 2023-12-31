1f09e34c935c5fad5e69af2927408ae142b9d995
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-01-01 07:34:24 +0900

    #32 feat: 타입별 헤더 제작

---
+++ app/components/Heading/TypeHeader.tsx
@@ -0,0 +1,124 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import logo from "@/assets/images/logo/pd_logo.png";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { stepState } from "@/recoil/Account/atoms";
+import IconBtnWrapper from "../Button/IconBtnWrapper";
+import IconX from "@/assets/images/Icon-x.svg";
+import IconBell from "@/assets/images/Icon-bell.svg";
+import IconLeft from "@/assets/images/icon-left.svg";
+import IconSearch from "@/assets/images/icon-search.svg";
+
+export const TypeHeader = () => {
+  return <div>TypeHeader</div>;
+};
+
+//Close
+export const CloseHeader = () => (
+  <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center justify-end">
+    <IconBtnWrapper onClick={() => {}}>
+      <IconX />
+    </IconBtnWrapper>
+  </header>
+);
+
+// account header => 페이지가 아니라 step인데 이 부분은 어떻게 처리 해야하는가
+// 반려동물 정보입력을 페이지 이동으로 시켜야하는가
+
+const AccountHeader = () => {
+  const router = useRouter();
+  const [step, setStep] = useRecoilState(stepState);
+
+  const handleClickRoute = () => {
+    if (step > 0 && step <= 2) {
+      return setStep((prev) => prev - 1);
+    } else if (step === 0) {
+      return router.back();
+    }
+  };
+
+  return (
+    <div className={`w-full flex ${step !== 0 ? "justify-between" : "justify-end"}`}>
+      {step !== 0 ? (
+        <button type="button" onClick={handleClickRoute} className="p-4">
+          <IconLeft />
+        </button>
+      ) : null}
+
+      <IconBtnWrapper onClick={() => router.push("/login")}>
+        <IconX />
+      </IconBtnWrapper>
+    </div>
+  );
+};
+
+//Back
+export const BackHeader = () => {
+  return (
+    <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 items-center">
+      <IconBtnWrapper onClick={() => {}}>
+        <IconLeft />
+      </IconBtnWrapper>
+    </header>
+  );
+};
+
+//BackClose
+export const BackCloseHeader = () => {
+  return (
+    <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center justify-between ">
+      <IconBtnWrapper onClick={() => {}}>
+        <IconLeft />
+      </IconBtnWrapper>
+      <IconBtnWrapper onClick={() => {}}>
+        <IconX />
+      </IconBtnWrapper>
+    </header>
+  );
+};
+
+//Alert
+export const AlertHeader = () => {
+  return (
+    <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center justify-between ">
+      <Link href="/" className="px-3">
+        <Image src={logo.src} alt="Pet diary logo" width={75} height={18} />
+      </Link>
+      <IconBtnWrapper onClick={() => {}}>
+        <IconBell />
+      </IconBtnWrapper>
+    </header>
+  );
+};
+
+//Interactive
+export const InteractiveHeader = () => {
+  return (
+    <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center justify-between ">
+      <Link href="/" className="px-3">
+        <Image src={logo.src} alt="Pet diary logo" width={75} height={18} />
+      </Link>
+      <div>
+        <IconBtnWrapper onClick={() => {}}>
+          <IconBell />
+        </IconBtnWrapper>
+        <IconBtnWrapper onClick={() => {}}>
+          <IconSearch />
+        </IconBtnWrapper>
+      </div>
+    </header>
+  );
+};
+
+//login
+export const LogoHeader = () => {
+  return (
+    <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center">
+      <Link href="/" className="px-3">
+        <Image src={logo.src} alt="Pet diary logo" width={75} height={18} />
+      </Link>
+    </header>
+  );
+};
--- app/components/Heading/index.tsx
+++ app/components/Heading/index.tsx
@@ -1,29 +1,34 @@
 "use client";
 
-import { HeaderMap, HeaderProps } from "@/types/header";
-import Link from "next/link";
+import { HeaderMap } from "@/types/header";
 import { usePathname } from "next/navigation";
-import React, { ReactNode, createContext, useContext, useEffect, useMemo } from "react";
-import Image from "next/image";
-import logo from "@/assets/images/logo/pd_logo.png";
+import React, { ReactNode } from "react";
+import {
+  AlertHeader,
+  LogoHeader,
+  CloseHeader,
+  InteractiveHeader,
+  BackHeader,
+  BackCloseHeader,
+} from "./TypeHeader";
 
 // 헤더 유형별 컴포넌트 정의
 const HeaderElement = {
-  Close: () => <div>Close</div>,
-  Back: () => <div>Back</div>,
+  Close: () => <CloseHeader />,
+  Back: () => <BackHeader />,
   Home: () => <LogoHeader />,
-  BackClose: () => <div>BackClose</div>,
-  Alert: () => <div>Alert</div>,
-  Interactive: () => <div>Interactive</div>,
+  BackClose: () => <BackCloseHeader />,
+  Alert: () => <InteractiveHeader />,
+  Interactive: () => <InteractiveHeader />,
   Default: () => <div>default</div>,
 };
 
 // 현재 경로에 따른 헤더 컴포넌트 매핑
 const headerMap: HeaderMap = {
-  "/1": HeaderElement.Close,
-  "/2": HeaderElement.Back,
+  "/account": HeaderElement.Close,
+  "/calender": HeaderElement.Back,
   "/login": HeaderElement.Home,
-  "/3": HeaderElement.BackClose,
+  "/4": HeaderElement.BackClose,
   "/": HeaderElement.Alert,
   "/5": HeaderElement.Interactive,
   //  경로 추가
@@ -38,21 +43,16 @@ const HeadingComponent = () => {
 };
 
 const Heading = ({ children }: { children: ReactNode }) => {
-  return <header>{children}</header>;
+  return (
+    // <header className="w-full md:max-w-3xl h-14 mx-auto px-2 py-1 flex items-center">{children}</header>
+    <header>{children}</header>
+  );
 };
 
 const Content = () => {
   const pathname = usePathname();
   const HeaderComponent = headerMap[pathname] || HeaderElement.Default; // 경로에 해당하는 헤더 컴포넌트 또는 기본값
   return <HeaderComponent />;
-};
-
-const LogoHeader = () => {
-  return (
-    <Link href="/" className="px-3">
-      <Image src={logo.src} alt="Pet diary logo" width={75} height={18} />
-    </Link>
-  );
 };
 
 Heading.Content = Content;
