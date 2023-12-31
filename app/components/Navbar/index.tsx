3e2bbbb0063fceb3fa9474d019acc06d1d066155
catca <kdg96g@gmail.com>
2023-12-31 22:41:31 +0900

    #45 feat: 네비게이션 바 아이콘 추가
    pathnode 에 따라 아이콘 다르게 보이게 추가

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { usePathname, useRouter } from "next/navigation";
+import IconHome from '@/assets/images/navbar/icon-home.svg';
+import IconCalendar from '@/assets/images/navbar/icon-calendar.svg';
+import IconInfo from '@/assets/images/navbar/icon-info.svg';
+import IconCommunity from '@/assets/images/navbar/icon-community.svg';
+import IconMypage from '@/assets/images/navbar/icon-mypage.svg';
+import IconSelectedHome from '@/assets/images/navbar/icon-selected-home.svg';
+import IconSelectedCalendar from '@/assets/images/navbar/icon-selected-calendar.svg';
+import IconSelectedInfo from '@/assets/images/navbar/icon-selected-info.svg';
+import IconSelectedCommunity from '@/assets/images/navbar/icon-selected-community.svg';
+import IconSelectedMypage from '@/assets/images/navbar/icon-selected-mypage.svg';
+
+interface NavItem {
+  type: 'home' | 'calendar' | 'info' | 'community' | 'mypage';
+  pathname: string;
+  push: string;
+  content: string;
+  icon: React.ComponentType<{}>;
+  selectedIcon: React.ComponentType<{}>;
+}
+
+const NAVBAR: NavItem[] = [
+  {type: 'home', pathname: '/', push: '/', content: '홈', icon: IconHome, selectedIcon: IconSelectedHome},
+  {type: 'calendar', pathname: '/calendar', push: '/calendar', content: '캘린더', icon: IconCalendar, selectedIcon: IconSelectedCalendar},
+  {type: 'info', pathname: '/info', push: '/info', content: '반려지식', icon: IconInfo, selectedIcon: IconSelectedInfo},
+  {type: 'community', pathname: '/community', push: '/community', content: '커뮤니티', icon: IconCommunity, selectedIcon: IconSelectedCommunity},
+  {type: 'mypage', pathname: '/mypage', push: '/mypage', content: '마이페이지', icon: IconMypage, selectedIcon: IconSelectedMypage},
+]
+
+const Navbar = () => {
+  const nowPathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <div className="fixed w-full bottom-0 h-16 bg-white flex flex-nowrap z-50">
+      {NAVBAR.map(({type, pathname, push, content, selectedIcon, icon}) => {
+        const Icon = pathname === nowPathname ? selectedIcon : icon;
+        return (
+          <div className='cursor-pointer' onClick={() => router.push(push)} key={pathname}>
+            <Icon />
+            <div>{content}</div>
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Navbar;
