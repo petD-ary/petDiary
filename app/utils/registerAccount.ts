13ed2b9489b021e8b9446630b47da7b0a154e121
Dorabang <psa1195@naver.com>
2024-01-03 23:00:23 +0900

    feat: 유저 정보 업데이트 기능 구현

--- app/utils/registerAccount.ts
+++ app/utils/registerAccount.ts
@@ -1,21 +1,30 @@
-import axios from 'axios';
-import React from 'react';
+import axios from '@/libs/axios';
 
-interface RegisterAccountProps {
-  userId: string;
-  email: string;
-  password: string;
+export interface RegisterAccountProps {
+  user: {
+    nickname: string;
+  };
+  pet: {
+    petType: string;
+    breed: string;
+    name: string;
+    gender: string;
+    neutered: boolean;
+    birthday?: string;
+    adoptionDate?: string;
+    weight: string;
+  };
 }
 
-const RegisterAccount = async (userInfo: RegisterAccountProps) => {
-  try {
-    const response = await axios.post(
-      'http://localhost:5000/api/users/signup',
-      userInfo
-    );
-    console.log('response', response);
-  } catch (error) {
-    console.error('계정 등록 중 오류 발생:', error);
-  }
+const RegisterAccount = (data: RegisterAccountProps) => {
+  const accsessToken = document.cookie;
+
+  axios
+    .post('/users/info', data, {
+      headers: { Authorization: `${accsessToken}` },
+      withCredentials: true,
+    })
+    .then((response) => console.log('response :', response));
 };
+
 export default RegisterAccount;
