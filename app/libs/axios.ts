9079a7cf6a6b0d8883bef14e5aba97791144ab13
Dorabang <psa1195@naver.com>
2024-01-03 15:41:00 +0900

    feat: credential instance 설정

--- app/libs/axios.ts
+++ app/libs/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  // withCredentials: true,
 });
+
+export const credentialsSettings = { withCredentials: true };
 
 export default instance;
