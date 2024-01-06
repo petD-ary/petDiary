008251be2d8da78cb4c07b0e581a175ce6ebc15a
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-01-06 17:23:17 +0900

    feat:home header

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,8 @@ import Walk from '@/components/Main/Walk';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { petInfoState } from '@/recoil/Main/atoms';
+import Heading from '@/components/Heading';
+import { MainAnimalHeader } from '@/components/Heading/TypeHeader';
 
 export default function Home() {
   const [petInfo, setPetInfo] = useRecoilState(petInfoState);
@@ -12,5 +14,30 @@ export default function Home() {
     setPetInfo({ name: '김콩', born: '1,004', together: '1,004' });
   }, []);
 
-  return <div className='max-w-[1500px] min-w-[300px] mx-auto p-14'></div>;
+  return (
+    <div className='max-w-[1500px] min-w-[300px] mx-auto p-14'>
+      <MainAnimalHeader />
+      {/* main header 
+       - animal count
+      - edit btn
+      */}
+      {/* main profile 
+       - profile
+      - name
+      - birth
+      - born
+      - together
+      */}
+      {/* location 
+       - my location title
+      - location name
+      */}
+      {/* weather & walk state 
+      - title
+      - walk state? ! icon
+      - weather & walk state 
+      - icon
+      */}
+    </div>
+  );
 }
--- app/components/Heading/TypeHeader.tsx
+++ app/components/Heading/TypeHeader.tsx
@@ -7,6 +7,8 @@ import IconX from '@/assets/images/Icon-x.svg';
 import IconBell from '@/assets/images/Icon-bell.svg';
 import IconLeft from '@/assets/images/icon-left.svg';
 import IconSearch from '@/assets/images/icon-search.svg';
+import IconEdit from '@/assets/images/icon-edit.svg';
+import { Caption, Title } from '../Typography/TypographyList';
 
 //Close
 export const CloseHeader = () => (
@@ -82,6 +84,25 @@ export const LogoHeader = () => {
       <Link href='/' className='px-3'>
         <Image src={logo.src} alt='Pet diary logo' width={logo.width} height={logo.height} />
       </Link>
+    </div>
+  );
+};
+
+//main
+export const MainAnimalHeader = () => {
+  const count = 3;
+  return (
+    <div className='header'>
+      <div className='flex gap-2 '>
+        <div className={`${Title.title2}`}>내 반려동물</div>
+        <div className={`text-primary-500 ${Title.title2}`}>{count}</div>
+      </div>
+      <div
+        className={`py-2 pl-2 pr-3 flex items-center gap-2 border border-secondary-100 rounded ${Caption.caption1} text-secondary-500 `}
+      >
+        <IconEdit />
+        수정
+      </div>
     </div>
   );
 };
