df0b80a9b9a2558dd05ebc0852b3ad6fd7517793
Dorabang <psa1195@naver.com>
2024-01-11 03:43:14 +0900

    chore: className값을 넣지 않을 경우에 기본값 설정

--- app/components/Container/index.tsx
+++ app/components/Container/index.tsx
@@ -5,8 +5,12 @@ interface ContainerProps {
   className?: string;
 }
 
-const Container: FC<ContainerProps> = ({ children, className }) => {
-  return <div className={`w-full md:max-w-3xl mx-auto px-5 ${className}`}>{children}</div>;
+const Container: FC<ContainerProps> = ({ children, className = '' }) => {
+  return (
+    <div className={`w-full md:max-w-3xl mx-auto px-5 ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
