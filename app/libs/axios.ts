0f356df2e0c53e1952bec31f4c6996f85e016fcd
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-01-15 16:35:21 +0900

    #53 fix: airkorea api error 수정

--- app/libs/axios.ts
+++ app/libs/axios.ts
@@ -4,9 +4,9 @@ export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
-
+// khaiValue
 export const fineDustApi = axios.create({
-  baseURL: '/B552584/ArpltnInforInqireSvc',
+  baseURL: 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc',
   params: {
     serviceKey: `${process.env.NEXT_PUBLIC_SERVICE_KEY}`,
     returnType: 'json',
--- app/utils/WalkingIndex.ts
+++ app/utils/WalkingIndex.ts
@@ -2,8 +2,8 @@ import { fineDustApi } from '@/libs/axios';
 
 export async function walkingIndex() {
   try {
-    const response = await fineDustApi('/getCtprvnRItmMesureDnsty');
-    console.log(response.data.khaiValue);
+    const response = await fineDustApi('/getCtprvnRltmMesureDnsty');
+    console.log(response);
     return response.data;
   } catch (error) {
     console.log(error);
