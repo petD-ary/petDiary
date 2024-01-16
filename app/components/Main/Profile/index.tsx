f5237c9aec7ef36e652e0823f9c44f2cb6daffa0
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-01-16 21:38:23 +0900

    #43 feat:일자 count util 사용

--- app/components/Main/Profile/index.tsx
+++ app/components/Main/Profile/index.tsx
@@ -9,23 +9,24 @@ import together from '@/assets/images/profile/together.png';
 import dog from '@/assets/images/profile/dog.jpeg';
 import { getPetData } from '@/utils/fetchPetData';
 import { PetData } from '@/types/petData';
+import { calculateElapsedDays, calculateRemainingDays } from '@/utils/calculateDay';
 
 const Profile = ({ user }: any) => {
   const [petData, setPetData] = useState<PetData[]>([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const petData = await getPetData();
-  //       console.log(petData, 'petData');
-  //       setPetData(petData);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const petData = await getPetData();
+        console.log(petData, 'petData');
+        setPetData(petData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
     <div className='mb-4 py-6 bg-white rounded-xl drop-shadow-[0_-4px_12px_rgba(0,0,0,0.04)]'>
@@ -34,11 +35,11 @@ const Profile = ({ user }: any) => {
           <div className='flex flex-col items-center gap-3 mb-4'>
             <div className='rounded-full overflow-hidden w-20 h-20 '>
               <Image src={dog} alt='profile' width={80} height={80} />
-              {user.nickname}
+              {item.name}
             </div>
             <div className='flex flex-row items-center gap-2'>
               <div className={`px-2 py-1 ${Extra} text-primary-500 bg-primary-50`}>
-                {item.createdAt}살
+                {calculateElapsedDays(item.createdAt)}살
               </div>
               <div className={`${Title.title3}`}>{item.name}</div>
             </div>
@@ -47,17 +48,17 @@ const Profile = ({ user }: any) => {
             <div className='text-center py-2 px-4'>
               <Image src={birth} alt='profile' width={44} height={44} />
               <span className={`${Caption.caption2} text-gray-500`}>생일</span>
-              <div className={`${Body.body2}`}>D-{item.birthday}</div>
+              <div className={`${Body.body2}`}>D-{calculateRemainingDays(item.adoptionDate)}</div>
             </div>
             <div className='text-center  py-2 px-4'>
               <Image src={born} alt='profile' width={44} height={44} />
               <span className={`${Caption.caption2}  text-gray-500`}>태어난지</span>
-              <div className={`${Body.body2}`}>{item.birthday}일</div>
+              <div className={`${Body.body2}`}>{calculateElapsedDays(item.adoptionDate)}일</div>
             </div>
             <div className='text-center  py-2 px-4'>
               <Image src={together} alt='profile' width={44} height={44} />
               <span className={`${Caption.caption2}  text-gray-500`}>함께한지</span>
-              <div className={`${Body.body2}`}>{item.adoptionDate}일</div>
+              <div className={`${Body.body2}`}>{calculateElapsedDays(item.adoptionDate)}일</div>
             </div>
           </div>
         </div>
--- app/libs/fetchUserAuthInfo.ts
+++ app/libs/fetchUserAuthInfo.ts
@@ -2,15 +2,15 @@ import { cookies } from 'next/headers';
 import { NextRequest } from 'next/server';
 import { instance } from './axios';
 
-// export async function getUser() {
-//   const cookieStore = cookies();
-//   const accessToken = cookieStore.get('accessToken');
-//   try {
-//     const response = await instance('/users', {
-//       headers: { cookie: `accessToken=${accessToken?.value}` },
-//     });
-//     return response.data;
-//   } catch {
-//     return undefined;
-//   }
-// }
+export async function getUser() {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken');
+  try {
+    const response = await instance('/users', {
+      headers: { cookie: `accessToken=${accessToken?.value}` },
+    });
+    return response.data;
+  } catch {
+    return undefined;
+  }
+}
