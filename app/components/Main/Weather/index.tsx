2e42fa0228b43811382217b501215459448542c8
Dorabang <psa1195@naver.com>
2024-01-24 02:04:50 +0900

    refactor: 날씨 컴포넌트 리팩토링
    - 커스텀 훅 이용 및 데이터 패칭 함수 분리
    
    - Related to: #52

--- app/components/Main/Weather/index.tsx
+++ app/components/Main/Weather/index.tsx
@@ -1,46 +1,32 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import WeatherCard from '../WeatherCard/WeatherCard';
-import axios from 'axios';
-import weatherDescKo from './weatherDescKo';
+import WeatherCard from './WeatherCard/WeatherCard';
+import useGeolocation from '@/hooks/useGeolocation';
+import { useEffect, useState } from 'react';
+import getWeatherData from './getWeatherData';
+
+export interface DataState {
+  weather: string;
+  icon: string;
+  temp: { main: string; max: string; min: string };
+}
 
 const Weather = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [weather, setWeather] = useState('');
-  const [temp, setTemp] = useState('');
-  const [icon, setIcon] = useState('');
+  const geolocation = useGeolocation();
+  const [data, setData] = useState<DataState | null>(null);
 
-  const onGeoOk = (position: any) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}&units=metric`;
-    axios.get(url).then((response) => {
-      const { data } = response;
-      const w_id = data.weather[0].id;
-      const koWeather = weatherDescKo[w_id];
-      const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-      setWeather(`${koWeather}`);
-      setTemp(`${Math.round(data.main.temp)}℃`);
-      setIcon(iconURL);
-      setIsLoading(false);
-    });
-  };
+  const conditions = !geolocation.position || !data;
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(onGeoOk);
-  }, []);
+    if (geolocation.position !== null && geolocation.error === null) {
+      getWeatherData(geolocation.position).then((res) =>
+        setData(res !== null ? res : null)
+      );
+    }
+  }, [geolocation]);
 
-  if (isLoading) return <WeatherCard title='오늘의 날씨' isLoading />;
+  if (conditions) return <WeatherCard isLoading={conditions} />;
 
-  return (
-    <WeatherCard
-      title='오늘의 날씨'
-      weather={weather}
-      temp={temp}
-      icon={icon}
-    />
-  );
+  return <WeatherCard data={data} />;
 };
 
 export default Weather;
