c77d5b5e0b7eeb290caded2d0dbd279aadd06245
Dorabang <psa1195@naver.com>
2024-01-24 02:59:34 +0900

    design: flex > grid로 css 변경

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
 import Container from '@/components/Container';
+import CurrLocation from '@/components/Main/CurrLocation';
 import Weather from '@/components/Main/Weather';
 
 export default function Home() {
   return (
-    <Container className='bg-extra-divice-bg h-full'>
-      <div className='flex justify-between'>
-        <div className='w-[calc((100%_-_20px)/2)]'>
-          <Weather />
-        </div>
+    <Container className='bg-extra-divice-bg h-full grid grid-cols-1 gap-3'>
+      <CurrLocation />
+      <div className='grid grid-cols-2 gap-3'>
+        <Weather />
+        {/* 산책 지수 위치를 잡기 위해 임시로 복사 */}
+        <Weather />
       </div>
     </Container>
   );
