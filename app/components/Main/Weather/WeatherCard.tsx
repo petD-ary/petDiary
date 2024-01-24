fdcd76aa363c6e7e9b77319625282ab1312a57e3
Dorabang <psa1195@naver.com>
2024-01-24 15:04:56 +0900

    rename: 폴더 구조 변경

--- app/components/Main/Weather/WeatherCard.tsx
+++ app/components/Main/Weather/WeatherCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import { Caption, Title } from '@/constants/Typography/TypographyList';
-import { DataState } from '..';
+import { DataState } from '.';
+import { useModal } from '@/hooks/useModal';
+import { MODAL_TYPE } from '@/components/Modal';
 
 interface CardType {
   data?: DataState;
@@ -14,16 +16,23 @@ const WeatherCard: React.FC<CardType> = ({
   addition,
   isLoading = false,
 }) => {
+  const { addModal } = useModal();
+
   return (
-    <div className='bg-white rounded-xl p-4 cursor-pointer'>
-      <div className='flex flex-col justify-between'>
+    <div
+      className='bg-white rounded-xl p-4 cursor-pointer'
+      onClick={() => addModal(MODAL_TYPE.WEATHER)}
+    >
+      <div className='flex flex-col justify-between pb-2'>
         <div className='text-2xl'>
           <div className={`${Caption.caption2} text-text-primary`}>
             오늘의 날씨
           </div>
           <div
             className={`flex gap-2 text-text-title mt-1 ${Title.title3} ${
-              isLoading ? 'w-40 h-[22px] bg-grayColor-100 animate-pulse' : ''
+              isLoading
+                ? 'max-w-40 w-full h-[22px] bg-grayColor-100 animate-pulse'
+                : ''
             }`}
           >
             <p>{data?.temp.main}</p>
@@ -32,6 +41,7 @@ const WeatherCard: React.FC<CardType> = ({
         </div>
         {addition}
       </div>
+
       <div className='w-full flex justify-end'>
         <div
           className={`relative w-11 h-11 bg-grayColor-100 rounded-2xl ${
