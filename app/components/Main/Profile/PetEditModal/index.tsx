d2176f812007f6655fc6cb75cd0c020671f5eabd
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-01-28 08:05:20 +0900

    feat:Add petData add btn

--- app/components/Main/Profile/PetEditModal/index.tsx
+++ app/components/Main/Profile/PetEditModal/index.tsx
@@ -1,9 +1,29 @@
 import PetEditContent from '@/components/PetEditContent';
+import Button from '@/components/Button';
+import { Title } from '@/constants/Typography/TypographyList';
 
+export type PetData = {
+  name: string;
+  birth: string;
+  breed: string;
+};
+
+const petData: PetData[] = [
+  {
+    name: '콩이',
+    birth: '2020.09.24',
+    breed: '믹스견',
+  },
+];
 const PetEditModal = () => {
   return (
     <>
-      <PetEditContent />
+      <div className='flex gap-2 '>
+        <div className={`${Title.title2} text-text-title`}>내 반려동물</div>
+        <div className={`text-primary-500 ${Title.title2}`}>{petData.length}</div>
+      </div>
+      <PetEditContent petData={petData} />
+      <Button className='border-primary-600' children={'반려동물추가'} variant={'outlined'} />
     </>
   );
 };
--- app/components/PetEditContent/index.tsx
+++ app/components/PetEditContent/index.tsx
@@ -5,43 +5,46 @@ import Image from 'next/image';
 import { calculateElapsedDays } from '@/utils/calculateDay';
 import Button from '../Button';
 import { Caption, Extra, SubTitle } from '@/constants/Typography/TypographyList';
-const petData = {
-  name: '콩이',
-  birth: '2020.09.24',
-  breed: '믹스견',
+import { PetData } from '../Main/Profile/PetEditModal';
+type PetEditContentProps = {
+  petData: PetData[];
 };
-const PetEditContent = () => {
+const PetEditContent = ({ petData }: PetEditContentProps) => {
   return (
-    <div className='bg-white p-4 rounded border flex items-center justify-between'>
-      <div className='flex gap-3 items-center '>
-        <div className='rounded-full overflow-hidden  '>
-          <picture>
-            <source srcSet={dogPng.src} />
-            <Image src={dog} alt='profile' width={48} height={48} priority />
-          </picture>
-        </div>
-        <div>
-          <span className={`px-2 py-1 ${Extra} text-primary-500 bg-primary-50 rounded-md`}>
-            {petData.birth}
-          </span>
+    <>
+      {petData?.map((item: any) => (
+        <div key={item.name} className='bg-white p-4 rounded border flex items-center justify-between'>
+          <div className='flex gap-3 items-center '>
+            <div className='rounded-full overflow-hidden  '>
+              <picture>
+                <source srcSet={dogPng.src} />
+                <Image src={dog} alt='profile' width={48} height={48} priority />
+              </picture>
+            </div>
+            <div>
+              <span className={`px-2 py-1 ${Extra} text-primary-500 bg-primary-50 rounded-md`}>
+                {item.birth}
+              </span>
 
-          <div className='flex items-center gap-2  mt-[6px]'>
-            <div className={`${SubTitle.subTitle2}`}>{petData.name}</div>
-            <div className={`${Caption.caption2}`}>
-              {petData.breed}/ {calculateElapsedDays(petData.birth)}살
+              <div className='flex items-center gap-2  mt-[6px]'>
+                <div className={`${SubTitle.subTitle2}`}>{item.name}</div>
+                <div className={`${Caption.caption2}`}>
+                  {item.breed}/ {calculateElapsedDays(item.birth)}살
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <span>
-        <Button
-          className={`${Caption.caption1} py-[6px] px-3 rounded-full `}
-          children={'수정'}
-          variant={'blueContained'}
-        />
-      </span>
-    </div>
+          <span>
+            <Button
+              className={`${Caption.caption1} py-[6px] px-3 rounded-full `}
+              children={'수정'}
+              variant={'blueContained'}
+            />
+          </span>
+        </div>
+      ))}
+    </>
   );
 };
 
