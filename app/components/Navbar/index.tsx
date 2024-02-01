3865a3f749be5488cd750e7bef189acc24e77462
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-01 15:33:23 +0900

    rename:calendar name 오타 변경

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter } from 'next/navigation';
 import IconHome from '@/assets/images/navbar/icon-home.svg';
 import IconCalendar from '@/assets/images/navbar/icon-calendar.svg';
 import IconInfo from '@/assets/images/navbar/icon-info.svg';
@@ -21,12 +21,47 @@ interface NavItem {
 }
 
 const NAVBAR: NavItem[] = [
-  {type: 'home', pathname: '/', push: '/', content: '홈', icon: IconHome, selectedIcon: IconSelectedHome},
-  {type: 'calendar', pathname: '/calendar', push: '/calendar', content: '캘린더', icon: IconCalendar, selectedIcon: IconSelectedCalendar},
-  {type: 'info', pathname: '/info', push: '/info', content: '반려지식', icon: IconInfo, selectedIcon: IconSelectedInfo},
-  {type: 'community', pathname: '/community', push: '/community', content: '커뮤니티', icon: IconCommunity, selectedIcon: IconSelectedCommunity},
-  {type: 'mypage', pathname: '/mypage', push: '/mypage', content: '마이페이지', icon: IconMypage, selectedIcon: IconSelectedMypage},
-]
+  {
+    type: 'home',
+    pathname: '/',
+    push: '/',
+    content: '홈',
+    icon: IconHome,
+    selectedIcon: IconSelectedHome,
+  },
+  {
+    type: 'calendar',
+    pathname: '/calendar',
+    push: '/calendar',
+    content: '캘린더',
+    icon: IconCalendar,
+    selectedIcon: IconSelectedCalendar,
+  },
+  {
+    type: 'info',
+    pathname: '/info',
+    push: '/info',
+    content: '반려지식',
+    icon: IconInfo,
+    selectedIcon: IconSelectedInfo,
+  },
+  {
+    type: 'community',
+    pathname: '/community',
+    push: '/community',
+    content: '커뮤니티',
+    icon: IconCommunity,
+    selectedIcon: IconSelectedCommunity,
+  },
+  {
+    type: 'mypage',
+    pathname: '/mypage',
+    push: '/mypage',
+    content: '마이페이지',
+    icon: IconMypage,
+    selectedIcon: IconSelectedMypage,
+  },
+];
 
 const NAVBAR_BLACKLIST = ['/login', '/account'];
 
@@ -34,23 +69,33 @@ const Navbar = () => {
   const nowPathname = usePathname();
   const router = useRouter();
 
-  if(NAVBAR_BLACKLIST.includes(nowPathname)) {
+  if (NAVBAR_BLACKLIST.includes(nowPathname)) {
     return null;
   }
 
   return (
-      <div className="fixed w-full md:max-w-3xl bottom-0 h-16 bg-white flex z-20 px-5 left-1/2 -translate-x-1/2">
-        {NAVBAR.map(({type, pathname, push, content, selectedIcon, icon}) => {
-          const isSelected = pathname === nowPathname ? true : false;
-          const Icon = isSelected ? selectedIcon : icon;
-          return (
-            <div className="w-full flex justify-center items-center flex-col flex-wrap cursor-pointer" onClick={() => router.push(push)} key={pathname}>
-              <Icon />
-              <div className={`${isSelected ? "text-primary-500" : "text-grayColor-700"}`}>{content}</div>
+    <div className='fixed w-full md:max-w-3xl bottom-0 h-16 bg-white flex z-20 px-5 left-1/2 -translate-x-1/2'>
+      {NAVBAR.map(({ type, pathname, push, content, selectedIcon, icon }) => {
+        const isSelected = pathname === nowPathname ? true : false;
+        const Icon = isSelected ? selectedIcon : icon;
+        return (
+          <div
+            className='w-full flex justify-center items-center flex-col flex-wrap cursor-pointer'
+            onClick={() => router.push(push)}
+            key={pathname}
+          >
+            <Icon />
+            <div
+              className={`${
+                isSelected ? 'text-primary-500' : 'text-grayColor-700'
+              }`}
+            >
+              {content}
             </div>
-          )
-        })}
-      </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
