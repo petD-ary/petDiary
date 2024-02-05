93769c49ca765ff795e9522a3e91e09559f3c459
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-05 15:38:42 +0900

    refactor:date-fns 함수 활용

--- app/hooks/useCalendar.tsx
+++ app/hooks/useCalendar.tsx
@@ -1,60 +1,30 @@
-import getDaysInMonth from 'date-fns/getDaysInMonth';
-import React, { useState } from 'react';
+import {
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+} from 'date-fns';
 
-const DATE_MONTH_FIXER = 1;
-// 한 달을 표시하는 총 칸 수
-const CALENDER_LENGTH = 35;
-// 빈 칸 표시용
-const DEFAULT_TRASH_VALUE = 0;
-// 한 주의 일수
-const DAY_OF_WEEK = 7;
-const DAY_LIST = ['일', '월', '화', '수', '목', '금', '토'];
+const useCalendar = (currentDate = new Date()) => {
+  const startDay = startOfWeek(startOfMonth(currentDate));
+  const endDay = endOfWeek(endOfMonth(currentDate));
 
-const useCalendar = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  //   getDaysInMonth : 현재 날짜가 속한 달의 총 일수 계산
-  const totalMonthDays = getDaysInMonth(currentDate);
+  const days = eachDayOfInterval({ start: startDay, end: endDay });
 
-  // 이전 달 마지막 주에 표시될 빈 칸 수
-  const prevDayList = Array.from({
-    length: Math.max(0, currentDate.getDay() - DATE_MONTH_FIXER),
-  }).map(() => DEFAULT_TRASH_VALUE);
+  // 주별로 분할
+  const weeks: Date[][] = [];
+  let week: Date[] = [];
 
-  // 현재 달의 일자를 나타내는 배열
-  const currentDayList = Array.from({ length: totalMonthDays }).map(
-    (_, i) => i + DATE_MONTH_FIXER,
-  );
+  days.forEach((day, index) => {
+    week.push(day);
+    if ((index + 1) % 7 === 0 || index === days.length - 1) {
+      weeks.push(week);
+      week = [];
+    }
+  });
 
-  // 다음 달 시작 주에 표시될 빈 칸 수
-  const nextDayList = Array.from({
-    length: CALENDER_LENGTH - currentDayList.length - prevDayList.length,
-  }).map(() => DEFAULT_TRASH_VALUE);
-
-  // 최종 달력 data
-  const currentCalendarList = [
-    ...prevDayList,
-    ...currentDayList,
-    ...nextDayList,
-  ];
-
-  // 달력 데이터를 주 단위로 분할하여 2차원 배열 생성함
-  const weekCalendarList = currentCalendarList.reduce<number[][]>(
-    (acc, cur, idx) => {
-      const chunkIndex = Math.floor(idx / DAY_OF_WEEK);
-      if (!acc[chunkIndex]) {
-        acc[chunkIndex] = []; // 새 주를 시작할 때 새 배열 생성
-      }
-      acc[chunkIndex].push(cur); // 현재 날짜를 해당 주 배열에 추가
-      return acc;
-    },
-    [],
-  );
-
-  return {
-    weekCalendarList: weekCalendarList,
-    currentDate: currentDate,
-    setCurrentDate: setCurrentDate,
-  };
+  return { weeks };
 };
 
 export default useCalendar;
