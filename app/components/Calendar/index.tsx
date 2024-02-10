dfcce507d040be67aba2cd405f5970507bdc49cd
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-10 23:56:10 +0900

    #67 style: 조건부로 text-color 수정

--- app/components/Calendar/index.tsx
+++ app/components/Calendar/index.tsx
@@ -104,12 +104,12 @@ const CalendarForm = () => {
               return (
                 <div
                   key={dayIndex}
-                  className={`py-4 px-4  flex flex-col  justify-center items-center rounded-[4px] ${
-                    isToday(day) ? 'bg-primary-600/30' : ''
-                  }
-                  ${!isCurrentMonth(day) ? 'text-gray-800 text-opacity-20' : ''}
+                  className={`py-4 px-4  flex flex-col  justify-center items-center rounded-[4px] 
+                  ${isWeekend(day) ? 'text-error' : ' text-gray-800'} 
+                  ${isToday(day) ? 'bg-primary-600/30' : ''}
+                  ${!isCurrentMonth(day) ? ' text-opacity-20' : ''}
                   ${isSelectDay(day) ? 'bg-primary-600 text-grayColor-10' : ''}
-                  ${isWeekend(day) ? 'text-error' : ''}
+               
                 
                   `}
                   onClick={() => handleDayClick(day)}
