ac20caae7bfcf379d174d29a08a63036b0c2c7a0
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-11 14:54:45 +0900

    #59 style:모달 좌우 패딩

--- app/components/PetEdit/PetListModal/index.tsx
+++ app/components/PetEdit/PetListModal/index.tsx
@@ -38,61 +38,63 @@ const PetListModal = () => {
     <Modal type={MODAL_TYPE.PETEDITLIST} variant={MODAL_VARIANT.FULLCARD}>
       <Modal.Header title='' titleType='left-X' />
       <AddPetModal />
-      <div className='flex gap-2 mb-10'>
-        <div className={`${Title.title2} text-text-title`}>내 반려동물</div>
-        <div className={`text-primary-500 ${Title.title2}`}>
-          {petData.length}
+      <div className=' px-4'>
+        <div className='flex gap-2 mb-10 '>
+          <div className={`${Title.title2} text-text-title`}>내 반려동물</div>
+          <div className={`text-primary-500 ${Title.title2}`}>
+            {petData.length}
+          </div>
         </div>
-      </div>
-      {petData?.map((item: any) => (
-        <div
-          key={item.name}
-          className='bg-white p-4 rounded flex items-center justify-between'
-        >
-          <div className='flex gap-3 items-center p-4'>
-            <div className='rounded-full overflow-hidden  '>
-              <picture>
-                <source srcSet={dogPng.src} />
-                <Image
-                  src={dog}
-                  alt='profile'
-                  width={48}
-                  height={48}
-                  priority
-                />
-              </picture>
-            </div>
-            <div>
-              <span
-                className={`px-2 py-1 ${Extra} text-primary-500 bg-primary-50 rounded-md`}
-              >
-                {item.birth}
-              </span>
+        {petData?.map((item: any) => (
+          <div
+            key={item.name}
+            className='bg-white mb-2 p-4 rounded flex items-center justify-between border'
+          >
+            <div className='flex gap-3 items-center p-4'>
+              <div className='rounded-full overflow-hidden  '>
+                <picture>
+                  <source srcSet={dogPng.src} />
+                  <Image
+                    src={dog}
+                    alt='profile'
+                    width={48}
+                    height={48}
+                    priority
+                  />
+                </picture>
+              </div>
+              <div>
+                <span
+                  className={`px-2 py-1 ${Extra} text-primary-500 bg-primary-50 rounded-md`}
+                >
+                  {item.birth}
+                </span>
 
-              <div className='flex items-center gap-2  mt-[6px]'>
-                <div className={`${SubTitle.subTitle2}`}>{item.name}</div>
-                <div className={`${Caption.caption2}`}>
-                  {item.breed}/ {calculateElapsedDays(item.birth)}살
+                <div className='flex items-center gap-2  mt-[6px]'>
+                  <div className={`${SubTitle.subTitle2}`}>{item.name}</div>
+                  <div className={`${Caption.caption2}`}>
+                    {item.breed}/ {calculateElapsedDays(item.birth)}살
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <span>
-            <Button
-              className={`${Caption.caption1} py-[6px] px-3 rounded-full `}
-              children={'수정'}
-              variant={'blueContained'}
-            />
-          </span>
-        </div>
-      ))}
-      <Button
-        className='border-purple-600 mt-3'
-        children={'반려동물추가'}
-        variant={'outlined'}
-        onClick={() => addModal(MODAL_TYPE.PETADD)}
-      />
+            <span>
+              <Button
+                className={`${Caption.caption1} py-[6px] px-3 rounded-full `}
+                children={'수정'}
+                variant={'blueContained'}
+              />
+            </span>
+          </div>
+        ))}
+        <Button
+          className='border-purple-600 mt-3'
+          children={'반려동물추가'}
+          variant={'outlined'}
+          onClick={() => addModal(MODAL_TYPE.PETADD)}
+        />
+      </div>
     </Modal>
   );
 };
