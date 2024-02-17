82be568b2de009cb759d850a60a459ea77d5600d
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-17 23:00:48 +0900

    충돌해결

--- app/components/Account/PetInfoForm/UpdatedUserData.ts
+++ app/components/Account/PetInfoForm/UpdatedUserData.ts
@@ -16,12 +16,7 @@ export interface UpdatedUserDataProps {
   };
 }
 
-<<<<<<< HEAD
-const UpdatedUserData = async (data: UpdatedUserDataProps) => {
-  await axios
-    .post('/users/info', data)
-    .then((response) => console.log('response :', response));
-=======
+
 const updatedUserData = async (data: UpdatedUserDataProps) => {
   try {
     await axios
@@ -30,7 +25,7 @@ const updatedUserData = async (data: UpdatedUserDataProps) => {
   } catch (error) {
     return console.log('🚀 ~ UpdatedUserData ~ error:', error);
   }
->>>>>>> 00e40bac308497415948496094ab50cd44ee8502
+
 };
 
 export default updatedUserData;
