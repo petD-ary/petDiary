d045b32845937d10692e19b8b383b8799567cd63
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-03-03 05:53:07 +0900

    delete:dnd 라이브러리 삭제

--- app/components/Calendar/CalendarModal/index.tsx
+++ app/components/Calendar/CalendarModal/index.tsx
@@ -1,13 +1,23 @@
 import Modal, { MODAL_TYPE, MODAL_VARIANT } from '@/components/Modal';
-
+import { useState } from 'react';
+import { DraggableItem, DroppableArea, ItemTypes } from './DragAndDropCalendar';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 const CalendarModal = () => {
+  const [selectedItems, setSelectedItems] = useState<
+    { type: string; value: string | number }[]
+  >([]);
+
+  const handleDrop = (item: { type: string; value: string | number }) => {
+    // Handle the dropped item as needed
+    setSelectedItems((prevItems) => [...prevItems, item]);
+  };
+
   return (
     <Modal type={MODAL_TYPE.CALENDAR} variant={MODAL_VARIANT.HALFSLIDE}>
-    <Modal.Header title='' titleType='left-X' />
-    <div className='h-full p-5 flex justify-between items-center'>
-         ss
-      </div>
-      <Modal.Button children='확인'/>
+      <Modal.Header title='' titleType='left-X' />
+      <div className='h-full p-5 flex justify-between items-center'></div>
+      <Modal.Button children='확인' />
     </Modal>
   );
 };
