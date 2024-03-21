0b81f97c79bb9fd2d28790cbab6403584cf92613
Dorabang <psa1195@naver.com>
2024-03-22 02:40:04 +0900

    chore: 불필요한 콘솔 제거

--- app/api/walkingIndex.ts
+++ app/api/walkingIndex.ts
@@ -14,7 +14,6 @@ export async function walkingIndex(sido: string) {
         },
       },
     );
-    console.log(response, 'response');
 
     return response.data.response.body.items;
   } catch (error) {
