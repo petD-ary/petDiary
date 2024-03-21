a29c3b14ff154e022fac5ac1c023ad689f96aac8
Dorabang <psa1195@naver.com>
2024-03-22 02:39:39 +0900

    design: 자세히 보기 버튼 추가

--- app/components/Main/Weather/WeatherCard.tsx
+++ app/components/Main/Weather/WeatherCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import IconArrowRight from '@/assets/images/icon-arrow-right.svg';
 import { Caption, Title } from '@/constants/Typography/TypographyList';
 import { DataState } from '.';
 import { useModal } from '@/hooks/useModal';
@@ -19,14 +20,19 @@ const WeatherCard: React.FC<CardType> = ({
   const { addModal } = useModal();
 
   return (
-    <div
-      className='bg-white rounded-xl p-4 cursor-pointer'
-      onClick={() => addModal(MODAL_TYPE.WEATHER)}
-    >
+    <div className='bg-white rounded-xl p-4'>
       <div className='flex flex-col justify-between pb-2'>
         <div className='text-2xl'>
-          <div className={`${Caption.caption2} text-text-primary`}>
-            오늘의 날씨
+          <div
+            className={`${Caption.caption2} text-text-primary flex justify-between`}
+          >
+            <div>오늘의 날씨</div>
+            <div
+              className='w-5 h-5 flex justify-center items-center cursor-pointer'
+              onClick={() => addModal(MODAL_TYPE.WEATHER)}
+            >
+              <IconArrowRight />
+            </div>
           </div>
           <div
             className={`flex gap-2 text-text-title mt-1 ${Title.title3} ${
