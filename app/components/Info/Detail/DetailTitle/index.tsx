4435e3705c5ac1e00573bef97424d1588da0148c
Dorabang <psa1195@naver.com>
2024-06-04 23:30:24 +0900

    design: Label 컴포넌트로 디자인 수정

--- app/components/Info/Detail/DetailTitle/index.tsx
+++ app/components/Info/Detail/DetailTitle/index.tsx
@@ -1,28 +1,27 @@
-'use client';
+import Label from '@/components/Label';
 import React from 'react';
 interface DetailTitleProps {
   title: string;
   summary: string;
   type: string;
-  tag: string;
+  tag: '높음' | '보통' | '낮음';
 }
 
 const DetailTitle = ({ title, summary, type, tag }: DetailTitleProps) => {
   return (
-    <div className='pt-5 px-5 bg-white'>
+    <div className='pt-5 px-5 bg-white mb-2'>
       <div className='py-4'>
         <h3 className='text-title2 font-semibold text-text-title'>{title}</h3>
         <p className='text-body2 font-medium text-text-primary py-3'>
           {summary}
         </p>
-        <div className='flex items-center gap-1 text-caption2 font-medium text-text-primary'>
+        <div className='flex items-center gap-2 text-caption2 font-medium text-text-primary'>
           <span>{type}</span>
-          <span>·</span>
-          <span
-            className={`rounded px-1 py-[2px] ${tag === '높음' ? 'bg-error/5 text-error' : 'bg-success/5 text-success'}`}
+          <Label
+            variant={tag === '높음' ? 'red' : tag === '보통' ? 'blue' : 'green'}
           >
             위험도 {tag}
-          </span>
+          </Label>
         </div>
       </div>
     </div>
