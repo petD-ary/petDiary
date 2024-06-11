6461e4686b2cd93e732857474fb69006e95c41e9
Dorabang <psa1195@naver.com>
2024-06-11 20:27:07 +0900

    WIP: 질병 사전 query 값 추가로 필터 기능 추가 중(무한 스크롤 로직 변경 예정)

--- app/hooks/queries/useKnowledge.ts
+++ app/hooks/queries/useKnowledge.ts
@@ -1,14 +1,24 @@
 import { getKnowledgeDisease } from '@/apis/info';
 import { queryDiseaseKey } from '@/constants/queryKey';
-import { useQuery } from 'react-query';
+import { PetType, RiskType } from '@/recoil/Info/atoms';
+import { useInfiniteQuery } from 'react-query';
 
-export const useDisease = () => {
-  return useQuery([queryDiseaseKey], () => getKnowledgeDisease(), {
-    staleTime: 0,
-    cacheTime: 5 * 60 * 1000,
-    refetchOnMount: true,
-    refetchOnReconnect: true,
-    refetchOnWindowFocus: true,
-    refetchInterval: 5 * 60 * 1000,
-  });
+export const useDisease = (query: { petType: PetType; risk: RiskType }) => {
+  return useInfiniteQuery(
+    [queryDiseaseKey, query.petType, query.risk],
+    ({ pageParam = 1 }) =>
+      getKnowledgeDisease(query.risk, pageParam, 15, query.petType),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return !lastPage?.isEnd ? nextPage : false;
+      },
+      staleTime: 0,
+      cacheTime: 5 * 60 * 1000,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: true,
+      refetchInterval: 5 * 60 * 1000,
+    },
+  );
 };
