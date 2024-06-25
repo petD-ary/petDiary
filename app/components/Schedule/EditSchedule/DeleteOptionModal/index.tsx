dcae637d25b014d42a50ad86c603761c3bd1ddb4
Dorabang <psa1195@naver.com>
2024-06-26 00:17:31 +0900

    design: 반복 일정 수정 및 삭제 모달 디자인 수정

--- app/components/Schedule/EditSchedule/DeleteOptionModal/index.tsx
+++ app/components/Schedule/EditSchedule/DeleteOptionModal/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+
 import { ScheduleOption } from '@/apis/schedules';
 import Button from '@/components/Button';
 import Modal, { MODAL_TYPE, MODAL_VARIANT } from '@/components/Modal';
+import OptionButton from '../OptionButton';
+
+type SelectedType = 'onlyOne' | 'since' | 'all';
 
 const DeleteOptionModal = ({
   handleDeleteSchedule,
 }: {
   handleDeleteSchedule: (option: ScheduleOption) => Promise<void>;
 }) => {
+  const [selected, setSelected] = useState<SelectedType>('all');
+
   return (
     <Modal
       type={MODAL_TYPE.SCHEDULE_DELETE_OPTION}
@@ -15,24 +22,41 @@ const DeleteOptionModal = ({
     >
       <div className='flex flex-col px-5 py-4'>
         <p className='text-secondary-900 mx-auto text-center pt-10 pb-6'>
-          반복되는 일정입니다. 이 일정을 삭제하시겠습니까?
+          반복되는 일정입니다. 일정을 삭제하시겠습니까?
         </p>
-        <div
-          className={`flex justify-between p-4 gap-3
-        [&_li]:w-[calc((100%-12px)/2)]`}
-        >
-          <Button
-            variant='reset'
-            onClick={() => handleDeleteSchedule('onlyOne')}
+        <div className={`flex flex-col justify-between gap-3 pb-4`}>
+          <OptionButton
+            selected={selected === 'onlyOne'}
+            onClick={() => {
+              setSelected('onlyOne');
+            }}
           >
-            이 일정만 삭제
-          </Button>
-          <Button
-            variant='outlined'
-            className=''
-            onClick={() => handleDeleteSchedule('since')}
+            선택된 일정만
+          </OptionButton>
+
+          <OptionButton
+            selected={selected === 'since'}
+            onClick={() => {
+              setSelected('since');
+            }}
           >
-            이후 모든 일정 삭제
+            이후 일정
+          </OptionButton>
+          <OptionButton
+            selected={selected === 'all'}
+            onClick={() => {
+              setSelected('all');
+            }}
+          >
+            모든 일정
+          </OptionButton>
+        </div>
+        <div className='py-3'>
+          <Button
+            variant='contained'
+            onClick={() => handleDeleteSchedule(selected)}
+          >
+            확인
           </Button>
         </div>
       </div>
--- app/components/Schedule/EditSchedule/EditOptionModal/index.tsx
+++ app/components/Schedule/EditSchedule/EditOptionModal/index.tsx
@@ -1,35 +1,59 @@
+'use client';
+import React, { useState } from 'react';
+
 import { ScheduleOption } from '@/apis/schedules';
-import Button from '@/components/Button';
 import Modal, { MODAL_TYPE, MODAL_VARIANT } from '@/components/Modal';
-import React from 'react';
+import OptionButton from '../OptionButton';
+import Button from '@/components/Button';
+
+type SelectedType = 'onlyOne' | 'since' | 'all';
 
 const EditOptionModal = ({
   handleUpdatedSchedule,
 }: {
   handleUpdatedSchedule: (option: ScheduleOption) => Promise<void>;
 }) => {
+  const [selected, setSelected] = useState<SelectedType>('all');
+
   return (
     <Modal type={MODAL_TYPE.SCHEDULE_EDIT_OPTION} variant={MODAL_VARIANT.CARD}>
       <div className='flex flex-col px-5 py-4'>
         <p className='text-secondary-900 mx-auto text-center pt-10 pb-6'>
           반복되는 일정입니다.
         </p>
-        <div
-          className={`flex justify-between p-4 gap-3
-        [&_li]:w-[calc((100%-12px)/2)]`}
-        >
-          <Button
-            variant='reset'
-            onClick={() => handleUpdatedSchedule('onlyOne')}
+        <div className={`flex flex-col justify-between gap-3 pb-4`}>
+          <OptionButton
+            selected={selected === 'onlyOne'}
+            onClick={() => {
+              setSelected('onlyOne');
+            }}
           >
-            이 일정만 적용
-          </Button>
-          <Button
-            variant='outlined'
-            className=''
-            onClick={() => handleUpdatedSchedule('since')}
+            선택된 일정만
+          </OptionButton>
+
+          <OptionButton
+            selected={selected === 'since'}
+            onClick={() => {
+              setSelected('since');
+            }}
           >
-            이후 일정에 대해 적용
+            이후 일정
+          </OptionButton>
+          <OptionButton
+            selected={selected === 'all'}
+            onClick={() => {
+              setSelected('all');
+            }}
+          >
+            모든 일정
+          </OptionButton>
+        </div>
+        <div className='py-3'>
+          <Button
+            variant='contained'
+            onClick={() => handleUpdatedSchedule(selected)}
+          >
+            확인
           </Button>
         </div>
       </div>
