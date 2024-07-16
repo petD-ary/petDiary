0c99f13212deb28b99934d47e9737ae9e1cff5b8
catca <kdg96g@gmail.com>
2024-07-16 23:51:20 +0900

    chore: jest 설정 추가

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
+
+module.exports = createJestConfig(customJestConfig);
