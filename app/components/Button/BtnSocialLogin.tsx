9e3e6e3cadd00ea1290e0d72d9f43286b02c5962
Dorabang <psa1195@naver.com>
2024-08-29 21:52:04 +0900

    refactor: 로그인 권한 유무에 따른 리다이렉션 기능 수정

--- app/components/Button/BtnSocialLogin.tsx
+++ app/components/Button/BtnSocialLogin.tsx
@@ -1,19 +1,23 @@
+'use client';
 import React, { ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { ProviderType } from '../Login/Constant';
 
 interface BtnSocialLoginProps {
   children: ReactNode;
   className?: string;
-  onClick: (e?: any) => void;
+  href: string;
 }
 
-const BtnSocialLogin = ({
-  children,
-  className,
-  onClick,
-}: BtnSocialLoginProps) => {
+const BtnSocialLogin = ({ children, className, href }: BtnSocialLoginProps) => {
+  const router = useRouter();
+  const handleClickLogin = async () => {
+    router.push(href);
+  };
   return (
     <button
-      onClick={onClick}
+      onClick={handleClickLogin}
       className={`w-full py-2 flex justify-center items-center gap-1 rounded-md ${className}`}
     >
       {children}
--- app/utils/getToken.ts
+++ app/utils/getToken.ts
@@ -1,11 +1,29 @@
 'use server';
 import { cookies } from 'next/headers';
 
-const getToken = (name: string) => {
+export const getToken = (name: string) => {
   const cookieStore = cookies();
   const token = cookieStore.get(name);
 
   return token;
 };
 
-export default getToken;
+export interface SetTokenProps {
+  accessToken: string;
+  refreshToken: string;
+  status: 'active' | 'temporary';
+  accessTime: number;
+  refreshTime: number;
+}
+
+export const setToken = async (data: SetTokenProps) => {
+  const cookieStore = cookies();
+
+  cookieStore.set('accessToken', data.accessToken, {
+    maxAge: data.accessTime,
+  });
+
+  cookieStore.set('refreshToken', data.refreshToken, {
+    maxAge: data.refreshTime,
+  });
+};
