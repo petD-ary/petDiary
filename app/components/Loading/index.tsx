ca3aff361ebf3c3c2c2470e741667e475152f8f5
Dorabang <psa1195@naver.com>
2024-09-01 02:16:54 +0900

    feat: 로딩바 컴포넌트 추가

--- app/components/Loading/index.tsx
+++ app/components/Loading/index.tsx
@@ -1,6 +1,17 @@
+import './index.css';
+
 const Loading = () => {
   return (
-    <div className='border-4 border-primary-100 border-t-primary-500 rounded-full w-12 h-12 animate-spin' />
+    <svg className='spinner' viewBox='0 0 50 50'>
+      <circle
+        className='path'
+        cx='25'
+        cy='25'
+        r='20'
+        fill='none'
+        stroke-width='5'
+      />
+    </svg>
   );
 };
 
