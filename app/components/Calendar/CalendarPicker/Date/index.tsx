714e61f402b653aca7adced8045d4741305ae861
catca <kdg96g@gmail.com>
2024-09-12 23:50:28 +0900

    feat: 달력 오늘 표기 추가

--- app/components/Calendar/CalendarPicker/Date/index.tsx
+++ app/components/Calendar/CalendarPicker/Date/index.tsx
@@ -1,3 +1,4 @@
+import { SubTitle } from '@/constants/Typography/TypographyList';
 import { ReactNode } from 'react';
 
 interface DateProps {
@@ -31,20 +32,26 @@ const DateComponent = ({
       }}
       className={`max-w-[64px] aspect-square rounded-[4px]
       w-full h-full flex justify-center items-center
-      relative after:pb-[100%] after:block
-      cursor-pointer
-      ${isToday ? '!bg-primary-200/50 [&]:!text-gray-800 font-medium' : ''}
+      relative after:pb-[100%] after:block cursor-pointer
+      ${isSelected && 'bg-primary-600 !text-grayColor-10'}
+      ${isToday && '!bg-primary-50 border border-primary-100 [&]:!text-gray-800 font-medium'}
+      ${(isToday && isSelected) && 'border border-primary-600'}
+      ${!isCurrentMonth && '!text-opacity-20'}
       ${isSaturDay ? '!text-secondary-400' : 'text-gray-800'}
-      ${isSelected ? 'bg-primary-600 !text-grayColor-10' : ''}
       ${isSunDay ? '!text-error' : 'text-gray-800'} 
-  ${!isCurrentMonth ? '!text-opacity-20' : ''}
   `}
     >
       {/* 일정 표시 아이콘 */}
       {hasSchedule ? (
         <div className='absolute top-2 right-2 bg-accent h-2 w-2 rounded-full' />
       ) : null}
-      {children}
+      {isToday ? (
+          <div className='flex flex-col items-center'>
+            <div>{children}</div>
+            <div className={`text-primary-600 text-today`}>오늘</div>
+          </div>
+        )
+        : children}
     </div>
   );
 };
